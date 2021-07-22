0015380b363429da9d8d8d7a593aaf97cb0e7a0a
looliby <80762582+looliby@users.noreply.github.com>
2021-07-22 13:02:30 +0800

    Create grid布局.py

---
+++ 图形化编程/grid布局.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+win = tk.Tk()          #创建窗口
+win.title("Hello")
+label1 = tk.Label(win, text="用户名: ")
+label2 = tk.Label(win, text="密码: ")
+etUsername = tk.Entry(win)       # 编辑框
+etPassword = tk.Entry(win)
+label1.grid(row=0, column=0, padx=5, pady=5)   # Label放在0行0列，上下左右留白5像素
+label2.grid(row=1, column=0, padx=5, pady=5)
+etUsername.grid(row=0, column=1, padx=5, pady=5)
+etPassword.grid(row=1, column=1, padx=5, pady=5)
+btLogin = tk.Button(win, text="登录")
+btLogin.grid(row=2, column=0, columnspan=2, padx=5, pady=5)
+win.mainloop()      # 显示窗口
+
